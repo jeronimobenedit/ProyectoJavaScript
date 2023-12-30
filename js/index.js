4eabfcb80bd6da699ba6dab403161ad557a323af
jeronimo benedit <jerobenedit82@gmail.com>
2023-12-30 20:35:49 -0300

    agrego json y commit final

--- js/index.js
+++ js/index.js
@@ -4,55 +4,72 @@ function simularPrestamo() {
     const EDAD = document.getElementById("edad").value;
 
     if (EDAD < 18) {
-        alert("este sitio no es apto para menores de edad");
-        } else { 
-            const DNI = document.getElementById("dni").value;
-            if(DNI>46000000){
-                alert("este sitio no es apto para menores de edad");
+        alert("Este sitio no es apto para menores de edad");
+    } else {
+        const DNI = document.getElementById("dni").value;
+        if (DNI > 46000000) {
+            alert("Este sitio no es apto para menores de edad"); 
+        } else {
+            const infoUsuario = [NOMBRE, APELLIDO, EDAD, DNI];
+            alert(`Datos del usuario: ${infoUsuario}`);
+            const VALOR_PRESTAMO = parseFloat(document.getElementById("valorPrestamo").value);
+            const TIEMPO = parseFloat(document.getElementById("tiempodePago").value);
+            let monto_final = 0;
+            if (TIEMPO >= 24) {
+                monto_final = VALOR_PRESTAMO + (VALOR_PRESTAMO * 0.18);
+                alert(`Se aplicará una tasa de interés del 18%. Cumplido el plazo del préstamo, deberá devolver un monto total de ${monto_final}`);
             } else {
-                const infoUsuario = [NOMBRE, APELLIDO, EDAD, DNI];
-                alert(`Datos del usuario: ${infoUsuario}`);
-                const VALOR_PRESTAMO = parseFloat(document.getElementById("valorPrestamo").value);
-                const TIEMPO = parseFloat(document.getElementById("tiempodePago").value);
-                let monto_final = 0;
-                if (TIEMPO >= 24) {
-                    monto_final = VALOR_PRESTAMO + (VALOR_PRESTAMO * 0.18);
-                    alert(`Se aplicará una tasa de interés del 18%. Cumplido el plazo del préstamo, deberá devolver un monto total de ${monto_final}`);
-                } else {
-                    monto_final = VALOR_PRESTAMO + (VALOR_PRESTAMO * 0.12);
-                    alert(`Se aplicará una tasa de interés del 12%. Cumplido el plazo del préstamo, deberá devolver un monto total de ${monto_final}`);
-                    const respuesta_plan_de_pago = document.getElementById("plandePago").value;
-                    if (respuesta_plan_de_pago.toLowerCase() === "si") {
-                        const pago_mensual = monto_final / TIEMPO;
-                        const planDePago = [];
-                        for (let cuotas = 1; cuotas <= TIEMPO; cuotas++) {
-                            let deuda = monto_final - (pago_mensual * cuotas);
-                            planDePago.push({ cuota: cuotas, deudaRestante: deuda.toFixed(2)});
-                        }
-                        let planDesglosado = "Su plan de pago:\n";
-                        for (const cuotaInfo of planDePago) {
-                            planDesglosado += `Cuota ${cuotaInfo.cuota}: Deuda restante = ${cuotaInfo.deudaRestante}\n`;
-                        }
-                        alert(planDesglosado);
-                    } else {
-                        alert(`Usted deberá pagar la suma totl de ${monto_final} cumplido el plazo de ${TIEMPO} meses`);
+                monto_final = VALOR_PRESTAMO + (VALOR_PRESTAMO * 0.12);
+                alert(`Se aplicará una tasa de interés del 12%. Cumplido el plazo del préstamo, deberá devolver un monto total de ${monto_final}`);
+                const respuesta_plan_de_pago = document.getElementById("plandePago").value;
+                if (respuesta_plan_de_pago.toLowerCase() === "si") {
+                    const pago_mensual = monto_final / TIEMPO;
+                    const planDePago = [];
+                    for (let cuotas = 1; cuotas <= TIEMPO; cuotas++) {
+                        let deuda = monto_final - (pago_mensual * cuotas);
+                        planDePago.push({ cuota: cuotas, deudaRestante: deuda.toFixed(2) });
                     }
-                }    
-                const datosUsuario = {
-                    nombre: NOMBRE,
-                    apellido: APELLIDO,
-                    edad: EDAD,
-                    dni: DNI,
-                    valorPrestamo: VALOR_PRESTAMO,
-                    tiempoPago: TIEMPO,
-                    planPago: respuesta_plan_de_pago,
-                };
-                const datosGuardados = JSON.parse(localStorage.getItem("datosUsuarios")) || [];
-                datosGuardados.push(datosUsuario);
-                localStorage.setItem("datosUsuarios", JSON.stringify(datosGuardados));
-                alert("Datos guardados corretamente");
+                    let planDesglosado = "Su plan de pago:\n";
+                    for (const cuotaInfo of planDePago) {
+                        planDesglosado += `Cuota ${cuotaInfo.cuota}: Deuda restante = ${cuotaInfo.deudaRestante}\n`;
+                    }
+                    alert(planDesglosado);
+                } else {
+                    alert(`Usted deberá pagar la suma total de ${monto_final} cumplido el plazo de ${TIEMPO} meses`);
+                }
             }
+            const datosUsuario = {
+                nombre: NOMBRE,
+                apellido: APELLIDO,
+                edad: EDAD,
+                dni: DNI,
+                valorPrestamo: VALOR_PRESTAMO,
+                tiempoPago: TIEMPO,
+                planPago: respuesta_plan_de_pago,
+            };
+            const datosGuardados = JSON.parse(localStorage.getItem("datosUsuarios")) || [];
+            datosGuardados.push(datosUsuario);
+            localStorage.setItem("datosUsuarios", JSON.stringify(datosGuardados));
+            alert("Datos guardados correctamente");
         }
     }
-    document.getElementById("botonCalcularPrestamo").addEventListener("click", simularPrestamo);
+}
 
+function cargarDatosDesdeJSON() {
+    fetch('../datosUsuarios.json')
+        .then(response => response.json())
+        .then(data => {
+            const datosUsuario = data[0];
+
+            simularPrestamoDesdeJSON(datosUsuario);
+        })
+        .catch(error => {
+            console.error('Error al cargar datos desde el archivo JSON:', error);
+            alert('Error al cargar datos desde el archivo JSON');
+        });
+}
+
+function simularPrestamoDesdeJSON(datosUsuario) {
+}
+
+document.getElementById("botonCalcularPrestamo").addEventListener("click", simularPrestamo);
